6aa7c9b21b974e53e32e7f410b31f763bc3a400b
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2026-03-10 10:44:24 +0530

    Refactor SubjectsList component to include search functionality and improve layout

--- classroom-frontend/src/pages/subjects/list.tsx
+++ classroom-frontend/src/pages/subjects/list.tsx
@@ -1,8 +1,35 @@
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb";
+import { ListView } from "@/components/refine-ui/views/list-view";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+import { useState } from "react";
 
 const SubjectsList = () => {
+  const [searchQuery, setSearchQuery] = useState("");
   return (
-    <div>list</div>
-  )
-}
+    <ListView>
+      <Breadcrumb />
 
-export default SubjectsList
+      <h1 className="page-title">Subjects</h1>
+      <div className="intro-row">
+        <p>Quick access to essential metrics and management tools.</p>
+
+        <div className="actions-row">
+          <div className="search-field">
+            <Search className="search-icon" />
+
+            <Input
+              type="text"
+              placeholder="Search by name ..."
+              className="pl-10 w-full"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
+    </ListView>
+  );
+};
+
+export default SubjectsList;
